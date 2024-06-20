0fce2a206f66ce5056925739c38566674abcfaa4
bnyitrai03 <126956031+bnyitrai03@users.noreply.github.com>
2024-06-20 15:09:59 +0200

    HTTPS and new test images

--- Webszerver/app.py
+++ Webszerver/app.py
@@ -1,21 +1,26 @@
 from flask import Flask, jsonify
 import base64
 import datetime
+import random
 
 app = Flask(__name__)
 
 def get_base64_image():
     raw_image = emulated_camera()
     # Encode the image data to base64
-    encoded_image = base64.b64encode(raw_image).decode('utf-8')
-    return encoded_image
+    return base64.b64encode(raw_image).decode('utf-8')
 
 def emulated_camera():
+    # Selects the random image
+    num = random.randint(1,3)
+    images = {
+        1:'raspberry-pi-1.jpg',
+        2: 'raspberry-pi-2.jpg',
+        3: 'raspberry-pi-1.jpg'
+    }
     # Open the JPG image
-    with open('raspberry-pi-1.jpg', 'rb') as image_file:
-        # Read the image file
-        image_data = image_file.read()
-    return image_data
+    with open(images[num], 'rb') as image_file:
+        return image_file.read()
 
 def emulated_rtc():
     return datetime.datetime.now()
@@ -25,9 +30,9 @@ def send_image():
     response = {
         "image_data": get_base64_image(),
         "encoding": "base64",
-        "time": emulated_rtc().strftime("%m/%d/%Y, %H:%M:%S")
+        "time": emulated_rtc().strftime("%Y.%m.%d., %H:%M:%S")
     }
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run(ssl_context='adhoc', debug = True)
