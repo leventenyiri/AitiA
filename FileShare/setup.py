45a74d1fe4a24aaa923ecd358ee3d12f91a9a61a
Nyiri Levente <nyiri.levi88@gmail.com>
2024-07-01 17:55:03 +0200

    added workflows

--- FileShare/setup.py
+++ FileShare/setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'requests'
+        'requests',
+        'pytest'
     ],
     entry_points={
         'console_scripts': [
