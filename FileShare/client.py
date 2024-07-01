530b71c54c2bd177442e887109919151c3402499
bnyitrai03 <126956031+bnyitrai03@users.noreply.github.com>
2024-07-01 11:59:36 +0200

    Structrured client v1.0

--- FileShare/client.py
+++ FileShare/client.py
@@ -5,87 +5,85 @@ import logging
 from picamera2 import Picamera2
 from libcamera import controls
 
-picam2 = Picamera2()
+########## Config file data ###########
+path = '/mnt/nfs_share/picture.jpg'
+log_level = logging.DEBUG
+log_path = 'Test.log'
+quality = 95
+#####################################
 
-def configure_camera():
-    config = picam2.create_still_configuration()
-    picam2.configure(config)
-    # JPEG quality level: 0 - 95
-    picam2.options["quality"] = 95
-    # Use NULL preview
-    picam2.start(show_preview = False)
-    time.sleep(2)
-    #picam2.set_controls({"AfMode": controls.AfModeEnum.Continuos}) #Auto
-    # Start the focusing asynchronously
-    #focusing = picam2.autofocus_cycle(wait=False)
-
-def mount_nfs():
-    while True:
-        try:
-            # Run the mount command
-            result = subprocess.run(['sudo', 'mount', '/mnt/nfs_share'],
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    text=True,
-                                    check=True)
-            
-            # If mount was successful, break the loop
-            if result.returncode == 0:
-                logging.info("Mount successful.")
-                break
-
-        except subprocess.CalledProcessError as e:
-            logging.critical(f"An error occurred while mounting: {e}")
-            logging.critical(f"Error Output: {e.stderr}")
-            exit(1)
-
-        except Exception as e:
-            logging.critical(f"An error occurred while mounting: {e}")
-            exit(1)
+class Log:
+    def __init__(self, path, level):
+        self.path = path
+        self.level = level
         
-        time.sleep(1)    
-        logging.info("Retrying...")
-        
-def save_image(image_data):
-    path = '/mnt/nfs_share/picture.jpg'
-    
-    try:
-        # Save the image to the NFS share
-        with open(path, 'wb') as f:
-            f.write(image_data)
-            print(f"Image saved to {path}")
-            logging.info(f"Image saved to {path}")
-            
-    except FileNotFoundError as e: 
-        logging.critical(e)
-        exit(1)
-        
-counter = 0
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, filename='NFS_client.log', filemode='w',
+    def start(self):
+        logging.basicConfig(level=self.level, filename=self.path, filemode='w',
                         format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    logging.info("Start of the log")
-    #configure_camera()
-    start_time = time.time()
-    mount_nfs()
-    logging.info("Mount time: " + str(time.time() - start_time) + " seconds")
-    while(True):
-            #focusing = picam2.autofocus_cycle(wait=False)
-            #image_stream = io.BytesIO()
-            #metadata = picam2.capture_metadata()
-            #logging.info(metadata["ExposureTime"], metadata["AnalogueGain"])
-            
-            #if(picam2.wait(focusing)):
-            #picam2.capture_file(image_stream, format='jpeg')
-            #image_data = image_stream.getvalue()
-            #save_image(image_data)
-            image_filename = f'/mnt/nfs_share/picture_{counter:04d}.jpg'
-            picam2.capture_file(image_filename)
-            logging.info("Image saving time: " + str(time.time() - start_time) + " seconds") 
-            print(f"Image saved")
-            #else:
-                #logging.error("Couldn't focus!")
-            counter += 1
+        logging.info("Start of the log")
+
+class Camera:
+    def __init__(self, quality, path):
+        self.quality = quality
+        self.path = path
+        self.cam = Picamera2()  
+    
+    def start(self, quality):
+        config = picam2.create_still_configuration()
+        picam2.configure(config)
+        # JPEG quality level: 0 - 95
+        picam2.options["quality"] = quality
+        # Use NULL preview
+        picam2.start(show_preview = False)
+        time.sleep(2)
+        
+    def capture(self):
+        picam2.capture_file(self.path)
+        
+class App:
+    def __init__(self):
+        self.log = Log(log_path, log_level)
+        self.camera = Camera(quality, path)
+        
+    def mount_nfs(self):
+        while True:
+            try:
+                # Run the mount command
+                result = subprocess.run(['sudo', 'mount', '/mnt/nfs_share'],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        text=True,
+                                        check=True)
                 
-            time.sleep(10)
+                # If mount was successful, break the loop
+                if result.returncode == 0:
+                    logging.info("Mount successful.")
+                    break
+
+            except subprocess.CalledProcessError as e:
+                logging.critical(f"An error occurred while mounting: {e}")
+                logging.critical(f"Error Output: {e.stderr}")
+                exit(1)
+            
+            except Exception as e:
+                logging.critical(f"An error occurred while mounting: {e}")
+                exit(1)
+            
+            time.sleep(1)    
+            logging.info("Retrying...")
+            
+if __name__ == "__main__":
+    start_time = time.time()
+    
+    picam2 = Picamera2()
+    
+    # Need to replace to a deserialazable class/object
+    app = App(log_path, log_level, quality, path)
+    
+    app.log.start()
+    app.mount_nfs()
+    app.camera.start()
+    app.camera.capture()
+    
+    logging.info("Image saving time: " + str(time.time() - start_time) + " seconds")
+    print(f"Image saved")
