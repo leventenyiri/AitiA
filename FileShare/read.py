de37fe76ba6445c25b64d9107a7db296ade800ec
Nyiri Levente <nyiri.levi88@gmail.com>
2024-07-02 14:58:44 +0200

    fixing mistakes brought up by flake8

--- FileShare/read.py
+++ FileShare/read.py
@@ -7,29 +7,32 @@ import yaml
 log_path = 'log_config.yaml'
 config_path = 'config.json'
 
+
 class Log:
     def __init__(self, filepath):
         self.path = filepath
-        
+
     def start(self):
         with open(log_path, 'r') as f:
             config = yaml.safe_load(f)
         logging.config.dictConfig(config)
         logging.info("Start of the log")
 
+
 class Camera:
     def __init__(self, quality, path):
         self.quality = quality
         self.path = path
 
+
 class App:
     def __init__(self, config_path):
         # Read the config data to dictionaries
         camera_config = App.read_json_to_dict(config_path, ["Camera"])
-        
-        # Pass the config data 
+
+        # Pass the config data
         self.camera = Camera(camera_config['quality'], camera_config['path'])
-        
+
     @staticmethod
     def read_json_to_dict(path, keys):
         try:
@@ -51,9 +54,10 @@ class App:
             logging.error(f"Config file not found: {path}")
             raise FileNotFoundError(f"Config file not found: {path}")
 
+
 if __name__ == "__main__":
     log = Log(log_path)
     log.start()
     start_time = time.time()
-    
+
     app = App(config_path)
