61d9b12ebe9a6d18d29020ca1a441edd5fa48d65
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-02 14:43:43 +0200

    created log_config

--- FileShare/read.py
+++ FileShare/read.py
@@ -1,21 +1,21 @@
 import time
 import json
 import logging
+import logging.config
+import yaml
 
+log_path = 'log_config.yaml'
 config_path = 'config.json'
 
 class Log:
-    def __init__(self, filepath, level):
+    def __init__(self, filepath):
         self.path = filepath
-        self.level = level
         
     def start(self):
-        # Configure costume logging
-        
-        file_handler = logging.FileHandler(self.path)
-        log.addHandler(file_handler)
-        log.setLevel(self.level)
-        logging.info("Costume logging rules have been added")
+        with open(log_path, 'r') as f:
+            config = yaml.safe_load(f)
+        logging.config.dictConfig(config)
+        logging.info("Start of the log")
 
 class Camera:
     def __init__(self, quality, path):
@@ -24,12 +24,10 @@ class Camera:
 
 class App:
     def __init__(self, config_path):
-        # Read the config data to dictionariess
+        # Read the config data to dictionaries
         camera_config = App.read_json_to_dict(config_path, ["Camera"])
-        logging_config = App.read_json_to_dict(config_path, ["Log"])
         
         # Pass the config data 
-        self.log = Log(logging_config["path"], logging_config["level"])
         self.camera = Camera(camera_config['quality'], camera_config['path'])
         
     @staticmethod
@@ -40,8 +38,9 @@ class App:
             result = {}
             for key in keys:
                 if key not in data:
-                    logging.error(f"Key '{key}' not found in the config file")
-                    raise KeyError(f"Key '{key}' not found in the config file")
+                    msg = f"Key '{key}' not found in the config file"
+                    logging.error(msg)
+                    raise KeyError(msg)
                 result[key] = data[key]
             # Flatten the result dictionary for easier access
             return {k: v for d in result.values() for k, v in d.items()}
@@ -53,12 +52,8 @@ class App:
             raise FileNotFoundError(f"Config file not found: {path}")
 
 if __name__ == "__main__":
-    # Start the log
-    logging.basicConfig(level=logging.DEBUG, filemode='w',
-                            format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    log = logging.getLogger(__name__)
-    logging.info("Start of the log")
+    log = Log(log_path)
+    log.start()
     start_time = time.time()
     
     app = App(config_path)
-    app.log.start()
