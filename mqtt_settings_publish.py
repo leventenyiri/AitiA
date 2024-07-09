65d6242027a655cf1a492590a8b1358b15ca8b9c
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-09 16:26:09 +0200

    publish settings test file

---
+++ mqtt_settings_publish.py
@@ -0,0 +1,58 @@
+import time
+import json
+from paho.mqtt import client as mqtt_client
+
+broker = "192.168.0.103"
+port = 1883
+topic = "settings/er-edge"
+
+# Path to your config.json file
+config_path = "config.json"
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print(f"Failed to connect, return code {rc}")
+
+    client = mqtt_client.Client()
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    try:
+        with open(config_path, 'r') as file:
+            config_data = json.load(file)
+
+        # Convert the JSON object to a string
+        message = json.dumps(config_data)
+
+        result = client.publish(topic, message)
+        status = result[0]
+        if status == 0:
+            print(f"Sent config to topic {topic}")
+        else:
+            print(f"Failed to send message to topic {topic}")
+    except FileNotFoundError:
+        print(f"Config file not found: {config_path}")
+    except json.JSONDecodeError:
+        print(f"Invalid JSON in the config file: {config_path}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    time.sleep(1)  # Wait for the message to be published
+    client.disconnect()
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run()
