a0de84cb70645b7ca3732e863a417883b42bdb1c
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-10 12:59:41 +0200

    readded config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# Configuration file paths
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_CONFIG_PATH = os.path.join(SCRIPT_DIR, 'log_config.yaml')
+CONFIG_PATH = os.path.join(SCRIPT_DIR, 'config.json')
+
+# MQTT Configuration
+BROKER = "192.168.0.108"
+PORT = 1883
+PUBTOPIC = "mqtt/rpi/image"
+SUBTOPIC = "settings/er-edge"
