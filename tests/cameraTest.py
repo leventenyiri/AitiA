4192fe0ee296ed0f3c5e52d14948b89826a8b58a
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-18 15:43:30 +0200

    added Camera unit tests

--- tests/cameraTest.py
+++ tests/cameraTest.py
@@ -1,20 +1,52 @@
-# from mock_autogen import PytestMocker
-# from camera import Camera
+from camera import Camera
+import logging
+import unittest
+from unittest.mock import patch, MagicMock
 
-# PytestMocker(Camera).mock_everything().generate()
+Picamera2 = MagicMock()
+controls = MagicMock()
 
 
-""" def test_camera_init():
+class TestCamera(unittest.TestCase):
+    def setUp(self):
+        self.cam_config = {
+            'width': 1280,
+            'height': 720,
+            'quality': 90
+        }
+        self.basic_config = {
+            'manual_camera_settings_on': False
+        }
 
-    # Arrange: todo
-    basic_config = {
-        "quality": "4K",
-        "mode": "single-shot",
-        "period": 100,
-        "manual_camera_settings_on": False
-    }
-    cam_config = {
-        "quality": 95,
-        "width": 3840,
-        "height": 2160
-    } """
+    @patch('camera.Picamera2', Picamera2)
+    @patch('camera.controls', controls)
+    def test_init_manual_settings(self):
+        self.basic_config['manual_camera_settings_on'] = True
+        camera = Camera(self.cam_config, self.basic_config)
+        
+        self.assertEqual(camera.width, 1280)
+        self.assertEqual(camera.height, 720)
+        self.assertEqual(camera.quality, 90)
+
+    @patch('camera.Picamera2', Picamera2)
+    @patch('camera.controls', controls)
+    def test_init_3k_settings(self):
+        self.basic_config['quality'] = '3K'
+        camera = Camera(self.cam_config, self.basic_config)
+        
+        self.assertEqual(camera.width, 2560)
+        self.assertEqual(camera.height, 1440)
+        self.assertEqual(camera.quality, 95)
+    
+    @patch('logging.error')
+    @patch('camera.Picamera2', Picamera2)
+    @patch('camera.controls', controls)
+    def test_init_invalid_settings(self, mock_logging_error):
+        self.basic_config['quality'] = 'invalid'
+        camera = Camera(self.cam_config, self.basic_config)
+        
+        self.assertEqual(camera.width, 2560)
+        self.assertEqual(camera.height, 1440)
+        self.assertEqual(camera.quality, 95)
+        mock_logging_error.assert_called_once_with(f"Invalid quality specified: invalid. Defaulting to 3K quality.")
+        
