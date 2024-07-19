777325c4637f521371215240f73aa09ac20b498d
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-19 12:17:31 +0200

    tests for working_time_check function

--- tests/appTest.py
+++ tests/appTest.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from app import App
 from camera import Camera
 from mqtt import MQTT
@@ -16,3 +16,47 @@ def test_init_with_valid_json_config(app):
     assert isinstance(app.basic_config, dict)
     assert isinstance(app.camera, Camera)
     assert isinstance(app.mqtt, MQTT)
+
+
+@patch('utils.RTC.get_time', return_value='2024-07-19T10:00:00')
+@patch('utils.System.shutdown')
+def test_working_hours(mock_shutdown, mock_get_time, app):
+    app.basic_config = {
+        "wake_up_time": "06:00:00",
+        "shut_down_time": "20:00:00"
+    }
+    app.working_time_check()
+    mock_shutdown.assert_not_called()
+
+
+@patch('utils.RTC.get_time', return_value='2024-07-19T22:00:00')
+@patch('utils.System.shutdown')
+def test_shutdown_after_shut_down_time(mock_shutdown, mock_get_time, app):
+    app.basic_config = {
+        "wake_up_time": "06:00:00",
+        "shut_down_time": "20:00:00"
+    }
+    app.working_time_check()
+    mock_shutdown.assert_called_once()
+
+
+@patch('utils.RTC.get_time', return_value='2024-07-19T02:00:00')
+@patch('utils.System.shutdown')
+def test_shutdown_before_wake_up_time(mock_shutdown, mock_get_time, app):
+    app.basic_config = {
+        "wake_up_time": "06:00:00",
+        "shut_down_time": "20:00:00"
+    }
+    app.working_time_check()
+    mock_shutdown.assert_called_once()
+
+
+@patch('utils.RTC.get_time', return_value='2024-07-19T02:00:00')
+@patch('utils.System.shutdown')
+def test_night_mode(mock_shutdown, mock_get_time, app):
+    app.basic_config = {
+        "wake_up_time": "20:00:00",
+        "shut_down_time": "06:00:00"
+    }
+    app.working_time_check()
+    mock_shutdown.assert_not_called()
