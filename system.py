b55bad31631dc9f00ac5522ff67dc533dd2dd501
bnyitrai03 <bence.nyitrai@gmail.com>
2024-07-29 14:37:22 +0200

    commented system to ntp sync

--- system.py
+++ system.py
@@ -2,6 +2,7 @@ import subprocess
 import logging
 from datetime import datetime
 import pytz
+import time
 try:
     from gpiozero import CPUTemperature
 except ImportError:
@@ -47,16 +48,29 @@ class System:
 
 class RTC:
     @staticmethod
-    def sync():
+    def sync_RTC_to_system():
         try:
             subprocess.run(['sudo', 'hwclock', '--systohc'], check=True)
             logging.info(f"RTC synced to system clock")
         except subprocess.CalledProcessError as e:
             logging.error(f"Error syncing RTC: {e}")
 
-    def convert_timestamp(timestamp_line):
+    @staticmethod
+    def sync_system_to_ntp(max_retries=5, delay=2):
+        for retry in range(max_retries):
+            time.sleep(delay)
+            lines = RTC.get_timedatectl()
+            is_synced = RTC.find_line(lines, "System clock synchronized:")
+            if is_synced == "yes":
+                return True
+            logging.warning(f"Failed to sync system to NTP, retrying ({retry+1}/{max_retries})")
+            delay *= 2
+        return False
+
+    @staticmethod
+    def convert_timestamp(timestamp_str):
+        """Convert a timestamp string to ISO 8601 format."""
         try:
-            timestamp_str = timestamp_line.split(': ', 1)[1].strip()
             # Remove the 'UTC' part if it exists
             parts = timestamp_str.split()
             # if the last element is 'UTC' remove it
@@ -72,28 +86,40 @@ class RTC:
             exit(1)
 
     @staticmethod
+    def get_timedatectl():
+        """Get output from the 'timedatectl' command."""
+        result = subprocess.run(['timedatectl'], capture_output=True, text=True)
+        if result.returncode != 0:
+            raise Exception(f"Error getting date from timedatectl: {result.stderr}")
+        return result.stdout.splitlines()
+
+    @staticmethod
+    def find_line(lines, target_string):
+        """Find and return the line containing the target string."""
+        found_line = next(line for line in lines if target_string in line)
+        return found_line.split(': ', 1)[1].strip()
+
+    @staticmethod
     def get_time():
+        """Get the current time, ensuring synchronization with NTP and RTC."""
         try:
-            result = subprocess.run(['timedatectl'], capture_output=True, text=True)
-            if result.returncode != 0:
-                raise Exception(f"Error getting date from timedatectl: {result.stderr}")
+            # Get all the lines from timedatectl output
+            lines = RTC.get_timedatectl()
 
-            lines = result.stdout.splitlines()
-
-            """Check if system clock synchronized to the NTP servers
-            is_synced_line = next(line for line in lines if "System clock synchronized:" in line)
-            is_synced = is_synced_line.split(': ', 1)[1].strip()
-                if is_synced == "yes":  # in prod "no" !!!!
-                # Try to sync system clock to the NTP servers again??
-                logging.error("Couldn't sync system clock to the NTP servers, current time is not correct.")
-                exit(2) """
+            """  # Check if system clock synchronized to the NTP servers
+            is_synced = RTC.find_line(lines, "System clock synchronized:")
+            if is_synced == "no":
+                # Try to sync system clock to the NTP servers again
+                if not RTC.sync_system_to_ntp():
+                    logging.error("Couldn't sync system clock to the NTP servers, current time is not correct.")
+                    exit(1) """
 
             # Get the RTC time
-            rtc_time_line = next(line for line in lines if "RTC time:" in line)
+            rtc_time_line = RTC.find_line(lines, "RTC time:")
             rtc_datetime = RTC.convert_timestamp(rtc_time_line)
 
             # Get the time from the system clock
-            utc_time_line = next(line for line in lines if "Universal time:" in line)
+            utc_time_line = RTC.find_line(lines, "Universal time:")
             utc_datetime = RTC.convert_timestamp(utc_time_line)
 
             # If the RTC time is different from the system clock, sync the RTC
