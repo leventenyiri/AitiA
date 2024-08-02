a19162ee36378b4e1862cea327cc188516c4079b
GitHub Action <action@github.com>
2024-08-02 16:04:08 +0000

    Update version to 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='AitiA',
-    version='0.3.2',
+    version='0.3.3',
     description='Testing python code for Starling detection project',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
