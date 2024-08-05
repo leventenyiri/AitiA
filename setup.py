b27cea1befbc8073d8b6e247986468a927291398
bnyitrai03 <bence.nyitrai@gmail.com>
2024-08-05 13:20:30 +0200

    fixing folders

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='info@effective-range.com',
     packages=find_packages(where='AitiA'),
     package_dir={'': 'AitiA'},
-    scripts=['run_script.sh'],
+    scripts=['main.py'],
     install_requires=['pytest',
                       'PyYAML>=6.0',
                       'pillow',
