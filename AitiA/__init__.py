01d0419396cb8f4160afcfae5fdf79801ca38e48
bnyitrai03 <bence.nyitrai@gmail.com>
2024-08-07 12:02:57 +0200

    fixing front page

--- AitiA/__init__.py
+++ AitiA/__init__.py
@@ -1,9 +1,15 @@
 """
-# Sentinel MrHat Cam
-
 Welcome to the documentation.
 
-[Project Homepage](https://github.com/Aitia-IIOT/sentinel-mrhat-cam/blob/master/README.md)
+[Project Homepage](https://github.com/Aitia-IIOT/sentinel-mrhat-cam)
+
+README:
+
+https://raw.githubusercontent.com/Aitia-IIOT/sentinel-mrhat-cam/master/README.md?token=GHSAT0AAAAAACTY3S7LGMI6WQJN5WYWS6HYZVTIZFA
+
+Prototype schematic:
+
+https://github.com/bnyitrai03/rpizero_storage/blob/main/Protot%C3%ADpus%20rajz%20V1.3.png?raw=true
 """
 
 from .static_config import *
