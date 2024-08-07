26d4062f0532c2d0194b8cf081d89ce5da52daf4
bnyitrai03 <bence.nyitrai@gmail.com>
2024-08-07 12:19:41 +0200

    added readme to frontpage

--- AitiA/__init__.py
+++ AitiA/__init__.py
@@ -4,6 +4,57 @@ Welcome to the documentation.
 [Project Homepage](https://github.com/Aitia-IIOT/sentinel-mrhat-cam)
 
 ![Prototype Schematic](https://github.com/bnyitrai03/rpizero_storage/blob/main/Protot%C3%ADpus%20rajz%20V1.3.png?raw=true)
+
+# Sentinel MrHat Cam
+
+## How it works?
+
+### Overview
+
+TODO
+
+### Messaging
+
+**Subscribe topic:** `{username}/config` 
+
+```
+{
+    "quality":"3K",
+    "mode":"periodic",
+    "period":60, // in sec
+    "wakeUpTime":"06:00:00", // UTC
+    "shutDownTime":"19:00:00" // UTC
+}
+```
+
+- The device using a default configuration in case of config errors
+- TODO more decription
+
+**Publish topic:** `{username}/confirm` 
+
+`config-ok` OR `config-nok|{error desc}`
+
+- TODO description
+
+**Publish topic:** `{username}/image`
+
+```
+{
+     "timestamp":"2024-07-11T18:52:05.179690+00:00",
+     "image":"/9j/4AAQSkZJRgABAQAAAQAB...."
+     "cpuTemp":35.6,
+     "batteryTemp":48.5,
+     "batteryCharge": 95
+}
+```
+
+- TODO description
+
+**Publish topic:** `{username}/log`
+
+`log record text`
+
+- TODO descriptions
 """
 
 from .static_config import *
