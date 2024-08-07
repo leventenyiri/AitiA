41fa3232dbf55c2fc5320279db30a988496d6e62
bnyitrai03 <bence.nyitrai@gmail.com>
2024-08-07 10:36:21 +0200

    removed obsolete code

--- AitiA/utils.py
+++ AitiA/utils.py
@@ -2,8 +2,6 @@ import time
 import logging
 import logging.config
 import logging.handlers
-# import yaml
-# import os
 from functools import wraps
 
 
@@ -25,42 +23,3 @@ def log_execution_time(operation_name=None):
             return result
         return wrapper
     return decorator
-
-
-""" class Logger:
-    def __init__(self, filepath):
-        self.filepath = filepath
-
-    def start(self):
-        try:
-            if not os.path.exists(self.filepath):
-                raise FileNotFoundError(f"Log configuration file not found: {self.filepath}")
-            with open(self.filepath, 'r') as f:
-                config = yaml.safe_load(f)
-            logging.config.dictConfig(config)
-
-            # Manually create and add the MQTT handler
-            mqtt_handler = MQTTHandler()
-            mqtt_handler.setLevel(logging.DEBUG)
-            formatter = logging.Formatter('%(asctime)s -%(name)s - %(levelname)s - %(message)s')
-            mqtt_handler.setFormatter(formatter)
-            # Add the MQTT handler to the root logger
-            logging.getLogger().addHandler(mqtt_handler)
-            print("Log config from dict has loaded")
-
-            logging.info("Logging started")
-        except ImportError as e:
-            print(f"Import error: {e}")
-            print("This might be due to missing modules or incorrect import statements.")
-        except AttributeError as e:
-            print(f"Attribute error: {e}")
-            print("This might be due to missing attributes or methods in the MQTTHandler class.")
-        except TypeError as e:
-            print(f"Type error: {e}")
-            print("This might be due to incorrect method signatures or argument types.")
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            import traceback
-            traceback.print_exc()
-            exit(1)
- """
