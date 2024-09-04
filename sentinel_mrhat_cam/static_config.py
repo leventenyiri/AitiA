8f8caaa06ee0c8a9b7cdaeca474157a0efb4ac9c
leventenyiri <nyiri.levi88@gmail.com>
2024-09-04 17:26:11 +0200

    broker ip

--- sentinel_mrhat_cam/static_config.py
+++ sentinel_mrhat_cam/static_config.py
@@ -9,8 +9,8 @@ TEMP_CONFIG_PATH = os.path.join(SCRIPT_DIR, 'temp_config.json')
 STATE_FILE_PATH = os.path.join(SCRIPT_DIR, 'state_file.json')
 
 # MQTT Configuration
-# BROKER = "192.168.0.105"
-BROKER = "192.168.59.13"
+BROKER = "192.168.0.105"
+# BROKER = "192.168.59.13"
 PORT = 1883
 IMAGETOPIC = "mqtt/rpi/image"
 CONFIGACKTOPIC = "er-edge/confirm"
