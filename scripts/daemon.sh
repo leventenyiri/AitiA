440591c5966a80fd722e0960339b67a251100eb1
leventenyiri <nyiri.levi88@gmail.com>
2024-09-10 16:49:47 +0200

    commented out enable in daemon.sh

--- scripts/daemon.sh
+++ scripts/daemon.sh
@@ -22,12 +22,12 @@ EOF"
 # Reload the systemd daemon
 sudo systemctl daemon-reload
 
-# Enable the service to start on boot
-sudo systemctl enable sentinel_mrhat_cam.service
+# Enable the service to start on boot (rather do this manually, enabling it right away could cause problems)
+# sudo systemctl enable sentinel_mrhat_cam.service
 
 # sudo systemctl disable sentinel_mrhat_cam.service
 
 # Optional: Start the service immediately
 # sudo systemctl start sentinel_mrhat_cam.service
 
-echo "Service has been created and enabled successfully, reboot the system for the changes to take effect."
+echo "Service has been created successfully, enable it and reboot the system for the changes to take effect."
